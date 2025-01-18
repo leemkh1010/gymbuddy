9bb50a83323260a7f17a45f890279fd5ccb13e38
leemkh1993 <leemkh1993@gmail.com>
2025-01-18 11:51:45 +0800

    new: mongo table

--- services/pose-detect/src/repo/MongoCoreRepo.py
+++ services/pose-detect/src/repo/MongoCoreRepo.py
@@ -1,12 +1,14 @@
 from repo.Mongo import MongoDB
 from repo.Repo import CoreRepo
+from repo import ExerciseRepo, Exercise
+
 
 class MongoCoreRepo(MongoDB, CoreRepo):
-  def __init__(self, conn_str: str, db: str):
-    super().__init__(conn_str, db)
-    
-  def update_exercise(self, exercise):
-    pass
-  
-  def update_media(self, media):
-    pass
+    def __init__(self, conn_str: str, db: str):
+        super().__init__(conn_str, db)
+
+    def update_exercise(self, exercise: Exercise):
+        pass
+
+    def update_media(self, media):
+        pass
--- services/pose-detect/src/repo/Repo.py
+++ services/pose-detect/src/repo/Repo.py
@@ -3,32 +3,34 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
+
 class ExternalClient(ABC):
-  @abstractmethod
-  def connect(self) -> None:
-    pass
-  
-  @abstractmethod
-  def health_check(self) -> None:
-    pass
-  
-  @abstractmethod
-  def is_connected(self) -> bool:
-    pass
-  
-  @abstractmethod
-  def close() -> None:
-    pass
+    def __init__(self, conn_str: str):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.conn_str = conn_str
+
+    @abstractmethod
+    def connect(self) -> None:
+        pass
+
+    @abstractmethod
+    def health_check(self) -> None:
+        pass
+
+    @abstractmethod
+    def is_connected(self) -> bool:
+        pass
+
+    @abstractmethod
+    def close() -> None:
+        pass
+
 
 class CoreRepo(ABC):
-  def __init__(self, conn_str: str):
-    self.logger = logging.getLogger(self.__class__.__name__)
-    self.conn_str = conn_str
+    @abstractmethod
+    def update_exercise(self, exercise: dict) -> None:
+        pass
 
-  @abstractmethod
-  def update_exercise(self, exercise: dict) -> None:
-    pass
-  
-  @abstractmethod
-  def update_media(self, media: dict) -> None:
-    pass
+    @abstractmethod
+    def update_media(self, media: dict) -> None:
+        pass
